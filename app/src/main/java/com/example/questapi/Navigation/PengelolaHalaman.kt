5df893ff6d06c959a3b49e3494f1b5bf9b002cfd
Rangga857 <azharrangga28@gmail.com>
2024-12-30 08:18:37 +0700

    Create PengelolaHalaman sebagai pengatur navigasi antar halaman

---
+++ app/src/main/java/com/example/questapi/Navigation/PengelolaHalaman.kt
@@ -0,0 +1,44 @@
+package com.example.questapi.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questapi.ui.theme.View.DestinasiEntry
+import com.example.questapi.ui.theme.View.DestinasiHome
+import com.example.questapi.ui.theme.View.EntryMhsScreen
+import com.example.questapi.ui.theme.View.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                onDetailClick = {
+
+                    }
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
