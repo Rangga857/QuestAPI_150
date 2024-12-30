90b8d81248fde4ab2a02fe5f461426bbb2211e49
Rangga857 <azharrangga28@gmail.com>
2024-12-30 09:13:30 +0700

    create fungsi UpdateBody sebagai body updatemahasiswa

--- app/src/main/java/com/example/questapi/ui/theme/View/UpdateView.kt
+++ app/src/main/java/com/example/questapi/ui/theme/View/UpdateView.kt
@@ -4,14 +4,46 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.questapi.ui.theme.ViewModel.UpdateUiEvent
+import com.example.questapi.ui.theme.ViewModel.UpdateUiState
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onSiswaValueChange: (UpdateUiEvent) -> Unit,
+    onUpdateClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        UpdateFormInput(
+            updateUiEvent = updateUiState.updateUiEvent,
+            onValueChange = { updatedEvent ->
+                onSiswaValueChange(updatedEvent)
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onUpdateClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Update")
+        }
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
