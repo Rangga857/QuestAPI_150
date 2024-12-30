b1095eb30be03a675f4134505b56c549b7755c70
Rangga857 <azharrangga28@gmail.com>
2024-12-30 08:33:35 +0700

    Create Fungsi ItemDetailMhs untuk menampilkan detail mahasiswa dalam layout kartu.

--- app/src/main/java/com/example/questapi/ui/theme/View/DetailView.kt
+++ app/src/main/java/com/example/questapi/ui/theme/View/DetailView.kt
@@ -1,14 +1,74 @@
 package com.example.questapi.ui.theme.View
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.questapi.model.Mahasiswa
+
+@Composable
+fun ItemDetailMhs(
+    modifier: Modifier = Modifier,
+    mahasiswa: Mahasiswa
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+
+            ComponentDetailMhs(
+                judul = "Nama",
+                isinya = mahasiswa.nama
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ComponentDetailMhs(
+                judul = "NIM",
+                isinya = mahasiswa.nim
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ComponentDetailMhs(
+                judul = "Alamat",
+                isinya = mahasiswa.alamat
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ComponentDetailMhs(
+                judul = "Jenis Kelamin",
+                isinya = mahasiswa.jenisKelamin
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ComponentDetailMhs(
+                judul = "Kelas",
+                isinya = mahasiswa.kelas
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+    }
+}
+
 
 @Composable
 fun ComponentDetailMhs(
