73a417a5828a171c5d2dd93dcd456b0d658dfa62
Rangga857 <azharrangga28@gmail.com>
2024-12-30 08:59:48 +0700

    create fungsi konversi untuk UpdateUiEvent dan Mahasiswa.

--- app/src/main/java/com/example/questapi/ui/theme/ViewModel/UpdateViewModel.kt
+++ app/src/main/java/com/example/questapi/ui/theme/ViewModel/UpdateViewModel.kt
@@ -1,5 +1,25 @@
 package com.example.questapi.ui.theme.ViewModel
 
+import com.example.questapi.model.Mahasiswa
+
+fun UpdateUiEvent.toMahasiswa(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUpdateUiEvent(): UpdateUiEvent = UpdateUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 data class UpdateUiState(
     val updateUiEvent: UpdateUiEvent = UpdateUiEvent()
 )
