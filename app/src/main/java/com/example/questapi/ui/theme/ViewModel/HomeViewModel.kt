8b931e6685e8f66f66ecc7e8c47a2afe9ef0318c
Rangga857 <azharrangga28@gmail.com>
2024-12-30 07:40:49 +0700

    Implementasi method getMhs di HomeViewModel untuk mengambil data mahasiswa

--- app/src/main/java/com/example/questapi/ui/theme/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi/ui/theme/ViewModel/HomeViewModel.kt
@@ -4,8 +4,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi.model.Mahasiswa
 import com.example.questapi.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -17,4 +21,22 @@ class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
 
     var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
         private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+
 }
