a74565b43241ebc4ab718a6cc3b24b9a2240f704
Rangga857 <azharrangga28@gmail.com>
2024-12-30 07:20:42 +0700

    Implementasi NetworkMahasiswaRepository untuk CRUD mahasiswa

--- app/src/main/java/com/example/questapi/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/questapi/repository/MahasiswaRepository.kt
@@ -1,6 +1,8 @@
 package com.example.questapi.repository
 
 import com.example.questapi.model.Mahasiswa
+import com.example.questapi.service.MahasiswaService
+import java.io.IOException
 
 interface MahasiswaRepository{
     suspend fun getMahasiswa(): List<Mahasiswa>
@@ -8,4 +10,40 @@ interface MahasiswaRepository{
     suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
     suspend fun deleteMahasiswa(nim: String)
     suspend fun getMahasiswaById(nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository{
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim,mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete mahasiswa. HTTP Status code: ${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        try {
+            return mahasiswaApiService.getMahasiswaBynim(nim)
+        } catch (e: IOException) {
+            throw IOException("Failed to fetch mahasiswa with NIM: $nim. Network error occurred.", e)
+        }
+    }
+
 }
