a42e4711dc8fdef56460f6fa915ad1c178f72a5b
Rangga857 <azharrangga28@gmail.com>
2024-12-30 09:01:48 +0700

    create class UpdateViewModel (fungsi untuk memuat, memperbarui state, dan menyimpan data mahasiswa)

--- app/src/main/java/com/example/questapi/ui/theme/ViewModel/UpdateViewModel.kt
+++ app/src/main/java/com/example/questapi/ui/theme/ViewModel/UpdateViewModel.kt
@@ -1,7 +1,43 @@
 package com.example.questapi.ui.theme.ViewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi.model.Mahasiswa
+import com.example.questapi.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
+class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(UpdateUiState())
+        private set
+
+    fun loadMahasiswa(nim: String) {
+        viewModelScope.launch {
+            try {
+                val mahasiswa = mhs.getMahasiswaById(nim)
+                uiState = UpdateUiState(updateUiEvent = mahasiswa.toUpdateUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateMahasiswaState(updateUiEvent: UpdateUiEvent) {
+        uiState = UpdateUiState(updateUiEvent = updateUiEvent)
+    }
+
+    suspend fun updateMahasiswa(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(nim, uiState.updateUiEvent.toMahasiswa())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 fun UpdateUiEvent.toMahasiswa(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
